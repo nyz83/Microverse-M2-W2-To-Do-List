3bb124e373c026760e29ad4d7e0964b002528ebf
Nyi Ye Zin <nyiyezin11@gmail.com>
2023-11-30 11:40:32 +0630

    feat: add structure for the ToDo List

---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+/** @format */
+
+// eslint-disable-next-line import/no-extraneous-dependencies
+const tailwindcss = require('tailwindcss');
+
+// eslint-disable-next-line import/prefer-default-export
+module.exports = {
+  plugins: ['postcss-preset-env', tailwindcss],
+};
