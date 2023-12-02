284d915cd8da5b1574c9291cec15d3d6b00aae99
Nyi Ye Zin <nyiyezin11@gmail.com>
2023-12-02 21:22:47 +0630

    feat:implements user interaction

--- src/index.js
+++ src/index.js
@@ -1,27 +1,24 @@
 import './style.css';
+import {
+  sortArr,
+  loadFromStorage,
+  saveToStorage,
+  incrementProp,
+} from './utils.js';
 
 const todoList = document.querySelector('#todoList');
+const clearCompletedBtn = document.querySelector('#clearCompleted');
 const todoForm = document.querySelector('#todoForm');
 const todoInput = document.querySelector('#todoInput');
 const todoTemplate = document.querySelector('#todoTemplate');
 const STORAGE_KEY = 'todos';
 
-function loadTodos() {
-  return JSON.parse(localStorage.getItem(STORAGE_KEY)) || [];
-}
-
-let todos = loadTodos();
-
-function saveTodos() {
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(todos));
-}
-
 function renderTodo(todo) {
   const { id, desc, completed } = todo;
   const todoTemplateClone = todoTemplate.content.cloneNode(true);
   const todoItem = todoTemplateClone.querySelector('#todoItem');
-  const todoCheckbox = todoTemplateClone.querySelector('[data-list-item-checkbox]');
-  const todoText = todoTemplateClone.querySelector('[data-list-item-text]');
+  const todoCheckbox = todoTemplateClone.querySelector('#todoCheckbox');
+  const todoText = todoTemplateClone.querySelector('#todoText');
   todoItem.dataset.id = id;
   todoText.value = desc;
   todoText.disabled = todo.completed;
@@ -29,10 +26,14 @@ function renderTodo(todo) {
   todoList.appendChild(todoTemplateClone);
 }
 
-todos.forEach((todo) => renderTodo(todo));
+window.addEventListener('load', () => {
+  const todos = loadFromStorage(STORAGE_KEY);
+  todos.forEach((todo) => renderTodo(todo));
+});
 
 todoForm.addEventListener('submit', (e) => {
   e.preventDefault();
+  const todos = loadFromStorage(STORAGE_KEY);
   const todoDesc = todoInput.value;
   if (!todoDesc) return;
   const newTodo = {
@@ -40,32 +41,54 @@ todoForm.addEventListener('submit', (e) => {
     desc: todoDesc,
     completed: false,
   };
-  todos.push(newTodo);
+  const newTodos = [...todos, newTodo];
+  saveToStorage(STORAGE_KEY, newTodos);
   renderTodo(newTodo);
-  saveTodos();
   todoInput.value = '';
 });
 
 todoList.addEventListener('click', (e) => {
-  if (!e.target.matches('[data-button-delete]')) return;
+  const todos = loadFromStorage(STORAGE_KEY);
+  if (!e.target.matches('#todoDelete')) return;
   const parent = e.target.closest('#todoItem');
   const { id } = parent.dataset;
   parent.remove();
-  todos = todos.filter((todo) => todo.id !== id);
-  todos.forEach((todo, index) => {
-    todo.id = (index + 1).toString();
-  });
-  const sortedTodos = todos.sort((a, b) => a.id - b.id);
+  const filteredTodos = todos.filter((todo) => todo.id !== id);
+  const sortedTodos = sortArr(incrementProp(filteredTodos, 'id'), 'id');
   todoList.innerHTML = '';
   sortedTodos.forEach((todo) => renderTodo(todo));
-  saveTodos();
+  saveToStorage(STORAGE_KEY, sortedTodos);
 });
 
 todoList.addEventListener('input', (e) => {
-  if (!e.target.matches('[data-list-item-text]')) return;
+  const todos = loadFromStorage(STORAGE_KEY);
+  if (!e.target.matches('#todoText')) return;
   const parent = e.target.closest('#todoItem');
   const { id } = parent.dataset;
-  const todo = todos.find((todo) => todo.id === id);
+  const newTodos = [...todos];
+  const todo = newTodos.find((todo) => todo.id === id);
   todo.desc = e.target.value;
-  saveTodos();
+  saveToStorage(STORAGE_KEY, newTodos);
+});
+
+todoList.addEventListener('change', (e) => {
+  const todos = loadFromStorage(STORAGE_KEY);
+  if (!e.target.matches('#todoCheckbox')) return;
+  const parent = e.target.closest('#todoItem');
+  const todoText = parent.querySelector('#todoText');
+  const { id } = parent.dataset;
+  const newTodos = [...todos];
+  const todo = newTodos.find((todo) => todo.id === id);
+  todo.completed = e.target.checked;
+  todoText.disabled = todo.completed;
+  saveToStorage(STORAGE_KEY, newTodos);
+});
+
+clearCompletedBtn.addEventListener('click', () => {
+  const todos = loadFromStorage(STORAGE_KEY);
+  const completedTodos = todos.filter((todo) => todo.completed !== true);
+  const sortedTodos = sortArr(incrementProp(completedTodos, 'id'), 'id');
+  todoList.innerHTML = '';
+  sortedTodos.forEach((todo) => renderTodo(todo));
+  saveToStorage(STORAGE_KEY, sortedTodos);
 });
